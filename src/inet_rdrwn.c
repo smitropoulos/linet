5728718f894e1e5f89d87159225b96efaa0304e0
smitropoulos <smitropoulos@edps.gr>
2019-08-02 14:02:19 +0300

    Added host / ip resolver

--- src/inet_rdrwn.c
+++ src/inet_rdrwn.c
@@ -60,20 +60,3 @@ ssize_t writen(int fd, const void *buffer, size_t n) {
     }
     return totWritten; /* Must be 'n' bytes if we get here */
 }
-
-int createWOLpacket(const unsigned char *mac_address) {
-
-    unsigned char packet[PACKET_LENGTH];
-
-    for (int i = 0; i < 6; i++) {
-        packet[i] = 0xFF;
-    }
-
-    for (int i = 1; i <= 16; i++) {
-        for (int j = 0; j < 6; j++) {
-            packet[i * 6 + j] = mac_address[j];
-        }
-    }
-
-
-}
